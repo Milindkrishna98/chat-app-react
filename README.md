13ceb447ca4c1a477a921f1aae8dcbe850af7c19
Milindkrishna <40000951+Milindkrishna98@users.noreply.github.com>
2021-07-02 09:37:57 +0530

    Chat App

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# chat-app-react
-This is the react based chat application made by Milind Krishna
+# Realtime Chat Application
+
+### [Live Site](https://chat-app-jsmastery.netlify.app)
+
+![Chat Application](https://i.ibb.co/vDhx8Md/Whats-App-Image-2021-01-26-at-02-01-43.jpg)
+
+## Introduction
+This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '9f7c18f0-0789-4bca-b16f-680d70e1b7d3';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
